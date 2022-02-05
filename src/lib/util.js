e532fb39da3257681f121b139ea6d4fa901de36b
Chris Troutner <chris.troutner@gmail.com>
2022-02-05 07:30:42 -0800

    fix(tests): Using a fake wallet for unit tests

--- src/lib/util.js
+++ src/lib/util.js
@@ -4,15 +4,15 @@
   folder.
 */
 
-'use strict'
+"use strict";
 
 // const config = require('../../config')
-const fs = require('fs')
+const fs = require("fs");
 
-const config = require('../../config')
+const config = require("../../config");
 
 // Winston logger
-const wlogger = require('./wlogger')
+const wlogger = require("./wlogger");
 
 // const BCH = require('./bch')
 // const bch = new BCH()
@@ -20,115 +20,120 @@ const wlogger = require('./wlogger')
 // const TokenLiquidity = require('./token-liquidity')
 // const tokenApp = new TokenLiquidity()
 
-const STATE_FILE_NAME = `${__dirname.toString()}/../../state/state.json`
+const STATE_FILE_NAME = `${__dirname.toString()}/../../state/state.json`;
 
 class TLUtils {
   // constructor () {}
 
   // Round a number to 8 decimal places, the standard used for Bitcoin.
-  round8 (numIn) {
-    return Math.floor(numIn * 100000000) / 100000000
+  round8(numIn) {
+    return Math.floor(numIn * 100000000) / 100000000;
   }
 
   // Save the app state to a JSON file.
-  saveState (data) {
+  saveState(data) {
     try {
-      wlogger.silly('entering util.js/saveState().')
+      wlogger.silly("entering util.js/saveState().");
       // console.log(`saveState() data: ${JSON.stringify(data, null, 2)}`)
 
-      const filename = STATE_FILE_NAME
+      const filename = STATE_FILE_NAME;
 
       return new Promise((resolve, reject) => {
         fs.writeFile(filename, JSON.stringify(data, null, 2), function (err) {
           if (err) {
-            wlogger.error('Error in token-util.js/saveState(): ', err)
-            return reject(err)
+            wlogger.error("Error in token-util.js/saveState(): ", err);
+            return reject(err);
           }
 
-          wlogger.silly('Successfully saved to state.json')
+          wlogger.silly("Successfully saved to state.json");
 
           // console.log(`${name}.json written successfully.`)
-          return resolve()
-        })
-      })
+          return resolve();
+        });
+      });
     } catch (err) {
-      wlogger.debug('Error in token-util.js/saveState()')
-      throw err
+      wlogger.debug("Error in token-util.js/saveState()");
+      throw err;
     }
   }
 
   // Open and read the state JSON file.
-  readState () {
+  readState() {
     try {
       // Delete the cached copy of the data.
-      delete require.cache[require.resolve(STATE_FILE_NAME)]
+      delete require.cache[require.resolve(STATE_FILE_NAME)];
 
-      const data = require(STATE_FILE_NAME)
-      return data
+      const data = require(STATE_FILE_NAME);
+      return data;
     } catch (err) {
-      wlogger.debug('Error in util.js/readState()')
-      throw new Error(`Could not open ${STATE_FILE_NAME}`)
+      wlogger.debug("Error in util.js/readState()");
+      throw new Error(`Could not open ${STATE_FILE_NAME}`);
     }
   }
 
   // Opens the wallet file and returns the contents.
-  openWallet () {
+  openWallet() {
     try {
-      let walletInfo
+      let walletInfo;
 
-      if (config.NETWORK === 'testnet') {
-        walletInfo = require(`${__dirname.toString()}/../../wallet-test.json`)
+      if (process.env.TL_ENV === "test") {
+        walletInfo = require(`${__dirname.toString()}/../../test/unit/mocks/fake-wallet.json`);
+        return walletInfo;
+      }
+
+      if (config.NETWORK === "testnet") {
+        walletInfo = require(`${__dirname.toString()}/../../wallet-test.json`);
       } else {
-        walletInfo = require(`${__dirname.toString()}/../../wallet-main.json`)
+        walletInfo = require(`${__dirname.toString()}/../../wallet-main.json`);
       }
       // console.log(`walletInfo in slp: ${JSON.stringify(walletInfo, null, 2)}`)
 
-      return walletInfo
+      return walletInfo;
     } catch (err) {
-      throw new Error('wallet file not found')
+      throw new Error("wallet file not found");
     }
   }
 
-  sleep (ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+  sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
   // Check that all the environment variables are set and 'make sense' before
   // allowing the app to start.
-  checkEnvVars (configObj) {
+  checkEnvVars(configObj) {
     try {
-      console.log(`config: ${JSON.stringify(configObj, null, 2)}`)
+      console.log(`config: ${JSON.stringify(configObj, null, 2)}`);
 
       // Check email settings.
-      if (config.emailServer === 'mail.someserver.com') {
+      if (config.emailServer === "mail.someserver.com") {
         console.warn(
-          '\nWarning: Email server variables not set! Email notifications will not be sent.\n'
-        )
+          "\nWarning: Email server variables not set! Email notifications will not be sent.\n"
+        );
       }
 
       // Check required settings.
       if (!config.BCH145ADDR) {
-        throw new Error('BCH 145 derivation address not set!')
+        throw new Error("BCH 145 derivation address not set!");
       }
       if (!config.SLP145ADDR) {
-        throw new Error('SLP 145 derivation address not set!')
+        throw new Error("SLP 145 derivation address not set!");
       }
       if (!config.BCH245ADDR) {
-        throw new Error('BCH 245 derivation address not set!')
+        throw new Error("BCH 245 derivation address not set!");
       }
       if (!config.SLP245ADDR) {
-        throw new Error('SLP 245 derivation address not set!')
+        throw new Error("SLP 245 derivation address not set!");
       }
       if (!config.SLP_TOKEN_ID) {
-        throw new Error('SLP Token ID not set!')
+        throw new Error("SLP Token ID not set!");
       }
     } catch (err) {
       console.log(
         `Error in src/lib/util.js/checkEnvVars().\nDid you create a .env file?\nShutting down. Reason: \n${err.message}`
-      )
-      process.exit(1)
+      );
+      process.exit(1);
     }
   }
 }
 
-module.exports = TLUtils
+module.exports = TLUtils;
